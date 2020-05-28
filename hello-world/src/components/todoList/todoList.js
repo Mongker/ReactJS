a527099ad73316173b093c39bade8f8b1550ae13
Mongker <49313037+Mongker@users.noreply.github.com>
2020-05-28 10:39:09 +0700

    MongLV: update TodoList

--- hello-world/src/components/todoList/todoList.js
+++ hello-world/src/components/todoList/todoList.js
@@ -1,20 +1,51 @@
 import React, { Component } from "react";
+// firebase
+import database from "../../firebase/firebase";
 
 // css
 import "./styles/index.css";
-import { Row, Col, Button, Modal, Input } from "antd";
+import { Row, Col, Button, Modal, Input, DatePicker, Select } from "antd";
 
-// const 
+// const
+
+const { Option } = Select;
 const { TextArea } = Input;
+const { RangePicker } = DatePicker;
+
 class todoList extends Component {
   constructor(props) {
     super(props);
     console.log("Run constructor");
-    this.state = { visible: false };
+    this.state = {
+      visible: false,
+      listTodo: [],
+      id: 0,
+      cap_do: "",
+      ngay_bat_dau: "",
+      ngay_ket_thuc: "",
+      ten_cong_viec: "",
+      noi_dung: "",
+      trang_thai: "Open",
+    };
+  }
+
+  componentDidMount() {
+    console.log("Run componentDidMount todoList");
+    let username = localStorage.getItem("Name_Chat");
+    this.setState({ username });
+    database
+      .database()
+      .ref("/todoList")
+      .on("value", (snapshot) => {
+        if (snapshot.val() !== undefined && snapshot.val() !== null) {
+          this.setState({
+            listTodo: Object.values(snapshot.val()),
+          });
+        }
+      });
   }
 
   showModal = () => {
-    debugger;
     this.setState({
       visible: true,
     });
@@ -22,21 +53,83 @@ class todoList extends Component {
 
   handleOk = (e) => {
     console.log(e);
+    const {
+      id,
+      cap_do,
+      ngay_bat_dau,
+      ngay_ket_thuc,
+      ten_cong_viec,
+      noi_dung,
+      trang_thai,
+    } = this.state;
+    debugger;
+    database
+      .database()
+      .ref("/todoList")
+      .push({
+        capdo: cap_do,
+        id: id + Math.floor(Math.random() * 10000),
+        ngaybatdau: ngay_bat_dau,
+        ngayketthuc: ngay_ket_thuc,
+        tencongviec: ten_cong_viec,
+        noidung: noi_dung,
+        trangthai: trang_thai,
+      });
     this.setState({
       visible: false,
+      id: 0,
+      cap_do: "",
+      ngay_bat_dau: "",
+      ngay_ket_thuc: "",
+      ten_cong_viec: "",
+      noi_dung: "",
+      trang_thai: "Open",
     });
+    console.log("Run _sendMessage");
   };
 
   handleCancel = (e) => {
     console.log(e);
     this.setState({
       visible: false,
+      id: 0,
+      cap_do: "",
+      ngay_bat_dau: "",
+      ngay_ket_thuc: "",
+      ten_cong_viec: "",
+      noi_dung: "",
+      trang_thai: "Open",
+    });
+  };
+
+  handleDate = (date, dateString) => {
+    this.setState({
+      ngay_bat_dau: dateString ? dateString[0] : "",
+      ngay_ket_thuc: dateString ? dateString[1] : "",
+    });
+  };
+
+  handleSelect = (value) => {
+    this.setState({
+      cap_do: value,
+    });
+  };
+
+  onChangeTitle = (e) => {
+    const text = e.target.value;
+    this.setState({
+      ten_cong_viec: text,
+    });
+  };
+  onChangeNoiDung = (e) => {
+    const text = e.target.value;
+    this.setState({
+      noi_dung: text,
     });
   };
 
   render() {
-    console.log("Run Render");
-    const { visible } = this.state;
+    const { visible, listTodo } = this.state;
     debugger;
     return (
       <React.Fragment>
@@ -49,7 +142,23 @@ class todoList extends Component {
             <Col span="12">
               <div className={"centerList"}>
                 <h1 className={"title"}>Danh Sách Công Việc</h1>
-                <Row className={"row-scroll"}></Row>
+                <Row>
+                  <div
+                    style={{
+                      alignItems: "center",
+                      textAlign: "center",
+                      overflowY: "auto",
+                      height: "300px",
+                      width: "100%",
+                    }}
+                  >
+                    {listTodo.map((item) => (
+                      <div>
+                        <h1 style={{ color: "blue" }}>{item.tencongviec}</h1>
+                      </div>
+                    ))}
+                  </div>
+                </Row>
               </div>
               <Row style={{ paddingTop: "10px" }}>
                 <Button type={"primary"} onClick={this.showModal}>
@@ -63,28 +172,43 @@ class todoList extends Component {
               onOk={this.handleOk}
               onCancel={this.handleCancel}
             >
-              <Row style={{paddingBottom: '2%'}}>
+              <Row style={{ paddingBottom: "2%" }}>
                 <Col span={5}>
                   <b>Tên nhiệm vụ:</b>{" "}
                 </Col>
                 <Col span={19}>
-                  <Input />
+                  <Input onChange={this.onChangeTitle} />
                 </Col>
               </Row>
-              <Row style={{paddingBottom: '2%'}}>
+              <Row style={{ paddingBottom: "2%" }}>
                 <Col span={5}>
                   <b>Nội dung:</b>{" "}
                 </Col>
                 <Col span={19}>
-                  <TextArea />
+                  <TextArea onChange={this.onChangeNoiDung} />
                 </Col>
               </Row>
-              <Row style={{paddingBottom: '2%'}}>
+              <Row style={{ paddingBottom: "2%" }}>
                 <Col span={5}>
-                  <b>Tên nhiệm vụ:</b>{" "}
+                  <b>Thời gian:</b>{" "}
                 </Col>
                 <Col span={19}>
-                  <Input />
+                  <RangePicker onChange={this.handleDate} />
+                </Col>
+              </Row>
+              <Row style={{ paddingBottom: "2%" }}>
+                <Col span={5}>
+                  <b>Cấp độ:</b>{" "}
+                </Col>
+                <Col span={19}>
+                  <Select
+                    defaultValue="Chọn Cấp Độ Công Việc"
+                    style={{ width: 200 }}
+                    onChange={this.handleSelect}
+                  >
+                    <Option value="1">Bình Thường</Option>
+                    <Option value="2">Khẩn Cấp</Option>
+                  </Select>
                 </Col>
               </Row>
             </Modal>
